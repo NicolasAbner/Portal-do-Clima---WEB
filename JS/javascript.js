1e171d0ea05820adf4f45507406ace184f6f38d6
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2024-09-26 00:34:36 -0300

    Side menu para mobile funcionando, fazendo o menu para 768px ou maior

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,15 +1,49 @@
 // ACORDEÃO HOME
-var acc = document.getElementsByClassName("accordion");
-var i;
+var acc = document.getElementsByClassName("accordion")
+var i
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-        } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-        } 
-    });
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active")
+    var panel = this.nextElementSibling
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px"
+    }
+  })
 }
+
+// MENU
+
+const buttonMenu = document.querySelector(".button-menu")
+const overlay = document.querySelector(".overlay")
+const sideMenu = document.querySelector(".side-menu")
+
+function openSideMenu() {
+  overlay.classList.toggle("menu-active")
+  sideMenu.classList.toggle("menu-active")
+  document.querySelector("body").style.overflow = "hidden"
+}
+
+function closeSideMenu() {
+  overlay.classList.toggle("menu-active")
+  sideMenu.classList.toggle("menu-active")
+  document.querySelector("body").style.overflow = "auto"
+}
+
+buttonMenu.addEventListener("click", (e) => {
+  openSideMenu()
+})
+
+document.addEventListener("click", (e) => {
+  if (e.target == overlay) {
+    closeSideMenu()
+  }
+})
+
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    closeSideMenu()
+  })
+})
