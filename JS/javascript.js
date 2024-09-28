906f5c539d22fe1d9411f93d879bf7cfa0b05737
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2024-09-27 22:15:28 -0300

    Merge manual já feito

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,17 +1,20 @@
 // ACORDEÃO HOME
-var acc = document.getElementsByClassName("accordion")
-var i
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active")
-    var panel = this.nextElementSibling
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px"
-    }
-  })
+function acordeon() {
+  var acc = document.getElementsByClassName("accordion")
+  var i
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active")
+      var panel = this.nextElementSibling
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px"
+      }
+    })
+  }
 }
 
 // MENU
